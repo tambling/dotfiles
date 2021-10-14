7f7ebe0ebb1da835106ac1efc0d6e1982cb1c457
Dean Tambling <tambling@github.com>
2021-10-13 19:41:04 -0700

    Spike: let's see if this configures correctly

--- install.sh
+++ install.sh
@@ -1,18 +1,8 @@
-if [ -x "$(command -v brew)" ]
-then
-  brew install rcm tmux fzf
-else
-  sudo apt update
-  sudo apt install rcm tmux fzf
-fi
+brew install rcm
 
-curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-npm i -g spaceship-prompt
-
-
-if [ ! -d "~/.dotfiles" ]; then
-  mkdir ~/.dotfiles
-  mv ./* ~/.dotfiles
+if [ ! -d "${HOME}/.dotfiles" ]; then
+  mkdir "${HOME}/.dotfiles"
+  mv "./*" "${HOME}/.dotfiles"
 fi
 
 rcup -f -x -- *.sh
