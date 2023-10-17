8da95135f52209fdd3ed80731c829dc8914faa75
Dean Tambling <tambling@github.com>
2023-10-16 19:04:23 -0700

    Install modern conveniences on startup

--- install.sh
+++ install.sh
@@ -5,6 +5,16 @@ shopt -s extglob
 
 sudo chsh -s $(which zsh) $USER
 
+if command -v brew > /dev/null
+then
+  package_manager="brew"
+elif command -v apt-get > /dev/null
+then
+  package_manager="apt-get"
+fi
+
+sudo $package_manager install neovim ripgrep tmux
+
 CURRENT_DIRECTORY="$(pwd -P)"
 
 for f in !(*.sh)
